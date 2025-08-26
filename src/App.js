3a3cdfa2335bd54ed5d8aef09ca713d9c5e92a2a
Ashwaq <ashwaqhussain651@gmail.com>
2025-08-26 22:48:50 +0530

    Added a fully functioning Top loading bar

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import 'bootswatch/dist/flatly/bootstrap.min.css';
 import Navbar from './components/MovieNavbar.js'
 import SearchBar from './components/SearchBar.js'
 import { BrowserRouter as Router, Routes, Route, Form } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 import About from "../src/components/aboutsection.js"
 
 
 export default function App() {
   const [switchMode,setswitchMode] = useState('light');
-
+  const [progress, setProgress] = useState(0);//create a top loading bar
   const Apikey = process.env.REACT_APP_API_KEY
   
   
@@ -31,9 +32,10 @@ export default function App() {
     <>
       <div className="container">
         <Router>
+            <div> <LoadingBar color="#f11946" progress={progress}/></div>
            <Navbar toggleMode={toggleMode} mode={switchMode}/>
           <Routes>
-             <Route path="/" element={<SearchBar Apikey={Apikey} mode={switchMode}/>}/> 
+             <Route path="/" element={<SearchBar setProgress_TLB={setProgress} Apikey={Apikey} mode={switchMode}/>}/> 
             {/* need to create seperate page of movies and tv shows */}
             <Route path="/movies" element={<h1>Movies Page</h1>} /> 
             <Route path="/tvshows" element={<h1>TV Shows Page</h1>} />
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,20 +2,20 @@ import { useState } from "react";
 import React from 'react'
 import { fetchMovies } from "../API/fetch";
 import { showAlert } from "./error_Alert";
-import {MovieCards} from './Movie_Cards'
+import {MovieCards} from './Movie_Cards';
 
 export default function SearchBar(props) {
   
     const [Searchitem,setSearchitem] = useState('');
     const [movies,setmovies] = useState([]); //array to store the movies imported from OMDB API
-    const [error, setError] = useState(""); //to handle errors
-    
+    const [error, setError] = useState(""); //to handle errors    
     const Key= props.Apikey;
     
     const onEnterSearchKey = (e) => {
       if (e.key === 'Enter') {
         e.preventDefault(); //to prevent the page from reloading everytime to prevent breaking components
         fetchMovies(Searchitem, setmovies, setError,Key); //send the movie name from searchbar to Fetch()to fetch that movie
+        props.setProgress_TLB(50) //sets the progress of loading bar to 50
       }
     }
 
@@ -38,6 +38,7 @@ export default function SearchBar(props) {
                         Showing {movies.length} movies for '{Searchitem}'
                       </p>
                       {/* get the necessary data like IMDBID from movies array to display in card component */}
+                      {props.setProgress_TLB(100)}
                       {movies.map((movie) => (
                         <MovieCards mode={props.mode} key={movie.imdbID} movie={movie} />
                       ))}
